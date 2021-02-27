f5a2aa5a6b62f5e75136ee1dea5e227a50d1977b
zxd <825995264@qq.com>
2021-02-27 17:04:19 +0800

    add code

---
+++ src/main/java/com/kruf/wow/mapper/OrderMapper.java
@@ -0,0 +1,16 @@
+package com.kruf.wow.mapper;
+
+import com.kruf.wow.pojo.Order;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author kruf
+ * @since 2021-02-27
+ */
+public interface OrderMapper extends BaseMapper<Order> {
+
+}
